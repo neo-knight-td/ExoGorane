bbdcaef2e979175c83e1313eabb00013977040bf
Thomas <thomas.dutrannois@outlook.com>
2023-01-16 12:21:18 +0100

    added Minimax Class

---
+++ Minimax.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <tuple>
+#include "GameState.cpp"
+#include "Strategy.cpp"
+
+using namespace std;
+
+class Minimax : public Strategy{
+    public :
+        tuple<int, int> getValueOfNextState(GameState gameState, int topOfTreeTeamId){
+            //default move & utility returned are -1
+            int valueOfNextState;
+            int moveToNextState;
+
+            //executed checks common to all strategies
+            std::tie(valueOfNextState,moveToNextState) = Strategy::getValueOfNextState(gameState, topOfTreeTeamId);
+            //if value of or/and move to next state is/are different that default value, these value should be returned (terminal state) 
+            if(valueOfNextState != this->defaultValueOfNextState || moveToNextState != this->defaultMoveToNextState)
+                return {valueOfNextState, moveToNextState};
+            
+
+            else {
+                gameState.generateSuccessors();
+
+                //select index of robot who has its turn
+                //int i = 0*gameState.friendTurn + 1*(!gameState.friendTurn);
+
+                //if its maximizer turn then return the value that maximizes the minimum gains of the opponent
+                if (gameState.teamId == topOfTreeTeamId){
+                    int value = -1000;
+                    for(list<GameState>::iterator it = (gameState.successors).begin(); it != (gameState.successors).end(); it++){
+                        //NOTE : "it" is an iterator (pointer). Need to add * to access successor object value
+                        int successorMinimax = get<0>(getValueOfNextState(*it, topOfTreeTeamId));
+
+                        if (successorMinimax > value){
+                            value = successorMinimax;
+                            moveToNextState = (*it).robots[gameState.teamId].location;
+                        }
+                    }
+                    return {value, moveToNextState};
+                }
+
+                //if its minimizer's turn then return the value that minimizes the maximums gains of the opponent
+                else {
+                    int value = 1000;
+                    for(list<GameState>::iterator it = (gameState.successors).begin(); it != (gameState.successors).end(); it++){
+                        //NOTE : "it" is an iterator (pointer). Need to add * to access successor object value
+                        int successorMinimax = get<0>(getValueOfNextState(*it, topOfTreeTeamId));
+
+                        if (successorMinimax < value){
+                            value = successorMinimax;
+                            moveToNextState = (*it).robots[gameState.teamId].location;
+                        }
+                    }
+                    return {value, moveToNextState};
+                }
+            }
+        }
+};
