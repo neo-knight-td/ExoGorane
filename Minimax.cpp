99e05b4d3d3c121795a4a0747ead4ad8dcbd67f1
Thomas <thomas.dutrannois@outlook.com>
2023-01-16 15:50:52 +0100

    modified so that that the least deep minimax is prefered in case of tie

--- Minimax.cpp
+++ Minimax.cpp
@@ -11,16 +11,17 @@ using namespace std;
 
 class Minimax : public Strategy{
     public :
-        tuple<int, int> getValueOfNextState(GameState gameState, int topOfTreeTeamId){
+        tuple<int, int, int> getValueOfNextState(GameState gameState, int topOfTreeTeamId){
             //default move & utility returned are -1
             int valueOfNextState;
             int moveToNextState;
+            int depthToTerminalState;
 
             //executed checks common to all strategies
-            std::tie(valueOfNextState,moveToNextState) = Strategy::getValueOfNextState(gameState, topOfTreeTeamId);
-            //if value of or/and move to next state is/are different that default value, these value should be returned (terminal state) 
+            std::tie(valueOfNextState,moveToNextState,depthToTerminalState) = Strategy::getValueOfNextState(gameState, topOfTreeTeamId);
+            //if value of or/and move to next state is/are different that default value, next state is terminal state 
             if(valueOfNextState != this->defaultValueOfNextState || moveToNextState != this->defaultMoveToNextState)
-                return {valueOfNextState, moveToNextState};
+                return {valueOfNextState, moveToNextState, depthToTerminalState};
             
 
             else {
@@ -32,31 +33,54 @@ class Minimax : public Strategy{
                 //if its maximizer turn then return the value that maximizes the minimum gains of the opponent
                 if (gameState.teamId == topOfTreeTeamId){
                     int value = -1000;
+                    int depth = 1000;
                     for(list<GameState>::iterator it = (gameState.successors).begin(); it != (gameState.successors).end(); it++){
                         //NOTE : "it" is an iterator (pointer). Need to add * to access successor object value
-                        int successorMinimax = get<0>(getValueOfNextState(*it, topOfTreeTeamId));
+                        int successorMinimax;
+                        int depthToMinimax;
+                        std::tie(successorMinimax, std::ignore, depthToMinimax) = getValueOfNextState(*it, topOfTreeTeamId);
 
                         if (successorMinimax > value){
                             value = successorMinimax;
                             moveToNextState = (*it).robots[gameState.teamId].location;
+                            depth = depthToMinimax;
+                        }
+                        //if 2 successors lead to same minimax value
+                        else if (successorMinimax == value){
+                            //check if depth to minimax from current successor is smaller
+                            if (depthToMinimax < depth){
+                                moveToNextState = (*it).robots[gameState.teamId].location;
+                                depth = depthToMinimax;
+                            }
                         }
                     }
-                    return {value, moveToNextState};
+                    return {value, moveToNextState, depth};
                 }
 
                 //if its minimizer's turn then return the value that minimizes the maximums gains of the opponent
                 else {
                     int value = 1000;
+                    int depth = 1000;
                     for(list<GameState>::iterator it = (gameState.successors).begin(); it != (gameState.successors).end(); it++){
                         //NOTE : "it" is an iterator (pointer). Need to add * to access successor object value
-                        int successorMinimax = get<0>(getValueOfNextState(*it, topOfTreeTeamId));
+                        int successorMinimax;
+                        int depthToMinimax;
+                        std::tie(successorMinimax, std::ignore, depthToMinimax) = getValueOfNextState(*it, topOfTreeTeamId);
 
                         if (successorMinimax < value){
                             value = successorMinimax;
                             moveToNextState = (*it).robots[gameState.teamId].location;
                         }
+                        //if 2 successors lead to same minimax value
+                        else if (successorMinimax == value){
+                            //check if depth to minimax from current successor is smaller
+                            if (depthToMinimax < depth){
+                                moveToNextState = (*it).robots[gameState.teamId].location;
+                                depth = depthToMinimax;
+                            }
+                        }
                     }
-                    return {value, moveToNextState};
+                    return {value, moveToNextState, depth};
                 }
             }
         }
--- Strategy.cpp
+++ Strategy.cpp
@@ -12,26 +12,29 @@ class Strategy{
     public :
         int const defaultValueOfNextState = -1;
         int const defaultMoveToNextState = -1;
+        int const defaultDepthToTerminalState = -1;
 
     public :
-        tuple<int, int> getValueOfNextState(GameState gameState, int teamId){
+        tuple<int, int, int> getValueOfNextState(GameState gameState, int teamId){
             int valueOfNextState = defaultValueOfNextState;
             int moveToNextState = defaultMoveToNextState;
+            int depthToTerminalState = defaultDepthToTerminalState;
 
             //if terminal state, return current state utility value & default next move (-1)
             if (gameState.isTerminalState()){
+                depthToTerminalState = gameState.depthOfState;
                 //TODO : next lines to be modified. Does not implement more than 2 robots
                 //TODO : ask on ExoLegend Discord if coins of dead robot count in team score
                 if (gameState.robots[teamId].isAlive){
                     //return the state utility (nb of coins gathered)
-                    return {gameState.robots[teamId].coinNb, moveToNextState};
+                    return {gameState.robots[teamId].coinNb, moveToNextState, depthToTerminalState};
                 }
                 else{
                     //if friendly robot died, terminal state utility is 0
-                    return {0, moveToNextState};
+                    return {0, moveToNextState, depthToTerminalState};
                 }
             }
 
-            return {valueOfNextState, moveToNextState};
+            return {valueOfNextState, moveToNextState, defaultDepthToTerminalState};
         }
 };
