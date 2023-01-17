af0726c71c6ee554aa74077dbdfbc6eb75e9c197
Thomas <thomas.dutrannois@outlook.com>
2023-01-17 21:13:11 +0100

    started modif of class strategy

--- Strategy.cpp
+++ Strategy.cpp
@@ -10,11 +10,15 @@ using namespace std;
 
 class Strategy{
     public :
-        int const defaultValueOfNextState = -1;
-        int const defaultMoveToNextState = -1;
-        int const defaultDepthToTerminalState = -1;
+        //TODO : could lead to a bug if default value are equal to real value outputed by utility of terminal state
+        int const defaultValueOfNextState = -1000;
+        int const defaultMoveToNextState = -1000;
+        int const defaultDepthToTerminalState = -1000;
 
     public :
+        tuple<int, int, int> exploreStateTree(GameState gameState, int teamId){
+            //this function needs to be redefined in each object that inherints from the Strategy class
+        }
         tuple<int, int, int> getValueOfNextState(GameState gameState, int teamId){
             int valueOfNextState = defaultValueOfNextState;
             int moveToNextState = defaultMoveToNextState;
@@ -26,13 +30,17 @@ class Strategy{
                 //TODO : next lines to be modified. Does not implement more than 2 robots
                 //TODO : ask on ExoLegend Discord if coins of dead robot count in team score
                 if (gameState.robots[teamId].isAlive){
-                    //return the state utility (nb of coins gathered)
-                    return {gameState.robots[teamId].coinNb, moveToNextState, depthToTerminalState};
+                    //return the state utility
+                    return {gameState.robots[teamId].coinNb - gameState.robots[(teamId+1)%2].coinNb, moveToNextState, depthToTerminalState};
                 }
                 else{
                     //if friendly robot died, terminal state utility is 0
                     return {0, moveToNextState, depthToTerminalState};
                 }
+            }
+
+            else{
+                this->exploreStateTree();
             }
 
             return {valueOfNextState, moveToNextState, defaultDepthToTerminalState};
