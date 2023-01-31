1da2e3662c18c65d751e8fc7065f208b9a9bac8e
thomas.dutrannois@outlook.com <thomas.dutrannois@outlook.com>
2023-01-31 17:25:37 +0100

    added max depth as a parameter

--- Minimax.cpp
+++ Minimax.cpp
@@ -7,6 +7,12 @@
 
 using namespace std;
 
+
+    //constructor for Minimax object
+    Minimax::Minimax(int paramMaxDepth){
+        this->maxDepth = paramMaxDepth;
+    }
+
     //getValueOfNextState is a recursive function that will explore the tree of states and return the maximum utility
     //value reachable from the current game state. It will also provide the move required to reach that value, the depth
     //at which that value appears in the tree & the depth to the ...
@@ -14,7 +20,7 @@ using namespace std;
         int moveToMinimax;
 
         //if terminal state
-        if (gameState.isTerminalState() || gameState.depthOfState >= MAX_DEPTH){
+        if (gameState.isTerminalState() || gameState.depthOfState >= maxDepth){
             int depthZero = 0;
             int dummyMove = -1;
             
--- Minimax.h
+++ Minimax.h
@@ -9,11 +9,13 @@ using namespace std;
 class Minimax
 {   protected :
         //maximum depth level that will be explored in the tree from the current game state
-        int const MAX_DEPTH = 11;
+        int maxDepth;
     public :
         //computes minimax value from current game state. getValueOfNextState also returns
         //the move required to reach Minimax and the depth to next state in which the utility goes up
         tuple<int, int, int, int> getValueOfNextState(GameState gameState, int topOfTreeTeamId);
+
+        Minimax(int paramMaxDepth);
 };
 
 #endif
